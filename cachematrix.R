bb0c7d28c037a9cd3534e6daac00b45ec61c9dfc
Veebug <50614523+Veebug@users.noreply.github.com>
2019-06-17 13:02:12 +0300

    correction

--- cachematrix.R
+++ cachematrix.R
@@ -16,30 +16,4 @@ cacheSolve <- function(x, ...) {
 
 
 
-# Make Cache Matrix
-makeCacheMatrix <- function(x = matrix()) {
-    i <- NULL
-  set <- function(y) {
-          x <<- y
-          i <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(inverse) i <<- inverse
-  getinverse <- function() i
-  list(set = set,
-       get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
-}
-# Cache Solve
-cacheSolve <- function(x, ...) {
-  i <- x$getinverse()
-  if (!is.null(i)) {
-          message("getting cached data")
-          return(i)
-  }
-  data <- x$get()
-  i <- solve(data, ...)
-  x$setinverse(i)
-  i
-}
+
